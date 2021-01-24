bbdcfb7aecc7865f0bbbdb5f7770ec0e205c31ce
Konstatos <k@rochev.ru>
2021-01-24 13:07:03 +0300

    Lesson 6 End

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Attack : MonoBehaviour
+    {
+        public bool Ok;
+        public int Atk;
+
+        [ContextMenu("Attack")]
+        void AttackTarget()
+        {
+            Atk = CanAttack() ? 1 : 2;
+            this.Info("Atk: " + Atk);
+        }
+
+        [ContextMenu("Attack", isValidateFunction: true)]
+        bool CanAttack()
+        {
+            return Ok;
+        }
+    }
+}
