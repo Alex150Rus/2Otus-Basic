6946667aa91dd75869ae6343a8eefc62108db00a
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-02-17 09:56:40 +0300

    created Abstract Entity

---
+++ Assets/Scripts/Entities/AbstractEntity.cs
@@ -0,0 +1,16 @@
+using Entitas;
+using UnityEngine;
+
+public abstract class AbstractEntity : MonoBehaviour
+{
+    protected Contexts contexts { get; private set; }
+    protected GameEntity entity { get; private set; }
+
+    protected virtual void Start()
+    {
+        contexts = Contexts.sharedInstance;
+        entity = contexts.game.CreateEntity();
+    }
+
+
+}
