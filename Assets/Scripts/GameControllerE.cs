331645f4d657c2519deb689107e5ca94141fd48b
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-02-17 12:27:10 +0300

    trying to undestand entitas, and starting from the beginning. Created Player Component and entity with this component in GameController

---
+++ Assets/Scripts/GameControllerE.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using Entitas;
+
+public class GameControllerE : MonoBehaviour
+{
+    Systems systems;
+
+    void Awake()
+    {
+
+
+        var contexts = Contexts.sharedInstance;
+        var entity = contexts.game.CreateEntity();
+        entity.isPlayer = true;
+
+        systems = new Systems();
+        //systems.Add(new DeathSystem(contexts));
+        //systems.Add(new PrefabInstantiateSystem(contexts));
+        //systems.Add(new ViewDestroySystem(contexts));
+        //systems.Add(new PlayerInputSystem(contexts));
+        //systems.Add(new ForwardMovementSystem(contexts));
+        //systems.Add(new ShotCollisionSystem(contexts));
+        //systems.Add(new TransformApplySystem(contexts));
+        systems.Initialize();
+    }
+
+    void OnDestroy()
+    {
+        systems.TearDown();
+    }
+
+    void Update()
+    {
+        systems.Execute();
+        systems.Cleanup();
+    }
+}
